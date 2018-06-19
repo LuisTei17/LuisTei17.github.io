ede7806d9f93e600300fb6b5460594d1c95a6e52
luis <luisedteixeira@gmail.com>
2018-06-19 16:32:37 -0300

    Initialized project

---
+++ scripts/app.js
@@ -0,0 +1,14 @@
+angular
+  .module('MyApp', ['ngMaterial'])
+  .controller('AppCtrl', function ($scope, $mdSidenav) {
+    $scope.toggleLeft = buildToggler('left');
+
+    function buildToggler(componentId) {
+      return function() {
+        $mdSidenav(componentId).toggle();
+        let botaoSide = document.getElementsByClassName(".sidenav-button");
+        
+      };
+    }
+
+  });
