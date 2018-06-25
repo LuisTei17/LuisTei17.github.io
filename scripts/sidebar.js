ed727c86d44e2f28ebe96c683308a47340ba2a2e
LuisTei17 <luisedteixeira@gmail.com>
2018-06-24 23:50:17 -0300

    added content of 'compras'

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,9 +1,9 @@
 $("#lentes").click(function() {
     let lista = `
                     <ul class="lista_categoria">
-                        <li><a class="main-link" href="index.html">Compras</a></li>
-                        <li><a class="main-link" href="index.html">Trocas</a></li>
-                        <li><a class="main-link" href="index.html">Bonificacao</a></li>                            
+                        <li><a class="main-link" href="example_lentes.html">Compras</a></li>
+                        <li><a class="main-link" href="">Trocas</a></li>
+                        <li><a class="main-link" href="">Bonificacao</a></li>                            
                     </ul>
                 `;
     $('.content-list').fadeOut("slow", function() {
