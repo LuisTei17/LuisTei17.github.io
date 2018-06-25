88f0d904d88b2d09468f15724151ad87e062677c
luis <luisedteixeira@gmail.com>
2018-06-25 09:20:33 -0300

    Added fields

--- scripts/app.js
+++ scripts/app.js
@@ -10,6 +10,42 @@ angular
 
     ];
 
+    $scope.formas = [
+      {nome:"Pontual"},
+      {nome:"Futura"},
+      {nome:"Remessa de futura"},
+      {nome:"Venda ordem"}
+
+    ];
+
+    $scope.tiposFaturamento = [
+      {nome:"Total"},
+      {nome:"Parcial"}
+
+    ];
+
+
+    $scope.condicoesPagamento = [
+
+      {
+        "SAP": [
+          {nome: "2x3"},
+          {nome: "0+23"},
+          {nome: "2+3"}
+          
+        ]
+      },
+      {
+        "Cartao": [
+          {nome:"Cartao de credito 1x"},
+          {nome:"Cartao de credito 2x"},
+          {nome:"Cartao de credito 3x"}
+          
+        ]
+      }
+    ];
+
+
    
     function buildToggler(componentId) {
       return function() {
